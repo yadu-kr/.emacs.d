fecc46a42f1f9e13cd97a3e6d9817970509ce554
M Variyar <ymvariya@amd.com>
2025-03-03 14:04:07 +0530

    Update config

--- custom-file.el
+++ custom-file.el
@@ -4,8 +4,12 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
- '(package-selected-packages '(evil company spacemacs-theme)))
+   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf"
+     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
+     default))
+ '(package-selected-packages
+   '(company dash doom-modeline evil-snipe nerd-icons-completion s
+	     spacemacs-theme undo-tree)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ elpa/spacemacs-theme-20241101.1030/spacemacs-theme-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "spacemacs-theme" "20241101.1030"
+  "Color theme with a dark and light versions."
+  ()
+  :url "https://github.com/nashamri/spacemacs-theme"
+  :commit "6c74684c4d55713c8359bedf1936e429918a8c33"
+  :revdesc "6c74684c4d55"
+  :keywords '("color" "theme"))
